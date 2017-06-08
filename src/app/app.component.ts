2b37e0fc06c1fdb703620b5035cf943d86e45bc5
dmayes403 <davidrmayes@gmail.com>
2017-06-07 22:19:46 -0600

    finished template forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,9 +12,32 @@ export class AppComponent {
   defaultQuestion = 'pet';
   answers = '';
   genders = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  }
+
+  submitted = false;
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: '',
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male',
+    // })    // <---- this all must be the exact same copy of the form value
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    });
   }
 
   // onSubmit(form: NgForm) {
@@ -22,6 +45,13 @@ export class AppComponent {
   // }
 
   onSubmit() {
-    console.log(this.signupForm)
+    this.submitted = true;
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
+
+    this.signupForm.reset();
   }
 }
